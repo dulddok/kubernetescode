11279f543a0c5c2f1f39bc6660fa39644d0a9a2b
reee <91013428+dulddok@users.noreply.github.com>
2022-07-29 16:23:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,77 +1,105 @@
-from flask import Flask, render_template, session, url_for, redirect, request
-import sqlite3
-conn = sqlite3.connect('database.db')
-print ("Opened database successfully")
-conn.execute('CREATE TABLE IF NOT EXISTS goorm (name TEXT, url TEXT)')
-print ("Table created successfully")
-con = sqlite3.connect("database.db")
-con.row_factory = sqlite3.Row
-cur = con.cursor()
-cur.execute("select * from goorm")
-rows = cur.fetchall()
-print("DB:")
-print(rows)
+from flask import Flask, render_template , request, redirect
 app = Flask(__name__)
-app.secret_key = 'this is super key'
-app.config['SESSION_TYPE'] = 'filesystem'
-userinfo = {'fu': 'fu'}
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+import datetime
+
+from models import db, Todo, Comment
+
+# app.py => sqlalchemy 설정
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# db에 app 연동
+db.init_app(app)
+
+# migrations
+migrate = Migrate(app,db)
 
 @app.route("/")
-def homepage():
-    if session.get('logged_in') :
-        return render_template('loggedin.html', rows = rows)
+def index():
+    todos = Todo.query.all()
+    return render_template("index.html", todos=todos)
+    
+# @app.route("/new")
+# def new():
+#     return render_template("new.html")
+
+# @app.route("/create", methods=["POST"])
+# def create():
+#     title = request.form["title"]
+#     deadline = request.form["deadline"]
+#     deadline = datetime.datetime.strptime(deadline,'%Y-%m-%d')
+#     todo = Todo(title=title, deadline=deadline);
+    
+#     db.session.add(todo)
+#     db.session.commit()
+    
+#     return redirect('/')
+    
+@app.route("/create", methods=["POST","GET"])
+def create():
+    if request.method == "POST":
+        # post방식으로 들어왔을 떄 -> form 태그에서 method="POST"로 왔을 떄
+        title = request.form["title"]
+        deadline = request.form["deadline"]
+        deadline = datetime.datetime.strptime(deadline,'%Y-%m-%d')
+        todo = Todo(title=title, deadline=deadline);
+        
+        db.session.add(todo)
+        db.session.commit()
+        
+        return redirect('/')
     else:
-        return render_template('index.html', rows = rows)
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        name = request.form['username']
-        password = request.form['password']
-        try:
-            if name in userinfo:
-                if userinfo[name] == password:
-                    session['logged_in'] = True
-                    return redirect(url_for('homepage'))
-                else:
-                    return 'Wrong Password!'
-            return 'ID does not exist'
-        except:
-            return 'Don\'t login'
+        # get방식으로 들어왔을 때 -> index.html에서 href로 왔을 때
+        return render_template("new.html")
+        
+@app.route("/<int:id>/delete")
+def delete(id):
+    todo = Todo.query.get(id)
+    db.session.delete(todo)
+    db.session.commit()
+    
+    return redirect('/')
+    
+@app.route("/<int:id>/update",methods=["POST","GET"])
+def update(id):
+    todo = Todo.query.get(id)
+    if request.method =="POST":
+        title = request.form["title"]
+        deadline = request.form["deadline"]
+        deadline = datetime.datetime.strptime(deadline,'%Y-%m-%d')
+        
+        todo.title = title
+        todo.deadline = deadline
+        
+        db.session.commit()
+        
+        return redirect('/')
+        
     else:
-        return render_template('login.html')
+        return render_template("update.html",todo=todo)
+        
+@app.route("/<int:id>")
+def read(id):
+    todo = Todo.query.get(id)
+    return render_template("read.html", todo=todo)
+    
+@app.route("/<int:id>/comment", methods=["POST"])
+def comment(id):
+    content = request.form["content"]
+    
+    comment = Comment(content=content)
+    
+    todo = Todo.query.get(id)
+    todo.comments.append(comment)
+    
+    db.session.add(comment)
+    db.session.commit()
+    
+    return redirect(f"/{id}")
 
-@app.route('/register', methods=['GET', 'POST'])
-def regist():
-    if request.method == 'POST':
-        userinfo[request.form['username']] = request.form['password']
-        return redirect(url_for('login'))
-    else:
-        return render_template('register.html')
-
-@app.route("/logout")
-def logout():
-    session['logged_in'] = False
-    return render_template('index.html')
-
-@app.route('/board')
-def loggedin_board():
-    return render_template('board.html', rows = rows) 
-
-@app.route('/add', methods = ['POST'])
-def add():
-    if request.method == 'POST':
-        try:
-            name = request.form['name']
-            url = request.form['url']
-            
-
-            with sqlite3.connect("database.db") as con:
-                cur = con.cursor()
-                cur.execute("INSERT INTO goorm (name, url) VALUES (?, ?)", (name, url))
-                con.commit()
-            
-        finally:
-            return render_template("board.html", rows = rows)
-            
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=8080,debug=True)
